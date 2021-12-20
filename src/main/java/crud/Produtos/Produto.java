29e86138c67e3daca87e38617750776fff3386a0
Phelyppe Alex <phelyppesilva@gmail.com>
2021-12-20 15:47:03 -0300

    adicionando db

--- src/main/java/crud/Produtos/Produto.java
+++ src/main/java/crud/Produtos/Produto.java
@@ -2,10 +2,23 @@ package crud.Produtos;
 
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+
+	@Column(nullable = false)
 	private String descricao;
+
+	@Column(nullable = false)
 	private String categoria;
 	
 	@Override
--- src/main/java/crud/Usuarios/Usuario.java
+++ src/main/java/crud/Usuarios/Usuario.java
@@ -2,11 +2,26 @@ package crud.Usuarios;
 
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String senha;
 
 	@Override
